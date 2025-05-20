71503997a8d0d88503935bc72932ae9b2df87a76
Sharandeep Kaur <Sharankgill22@gmail.com>
2025-05-20 15:02:50 +0530

    modifications for the directory and other corrections

--- Server/Server.js
+++ Server/Server.js
@@ -4,7 +4,9 @@ import cors from 'cors'
 import userRouter from './routes/userRoutes.js';
 import connectDB from './config/mongodb.js';
 import imageRouter from './routes/imageRoutes.js';
-
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const PORT = process.env.PORT || 4000
 const app = express();
@@ -14,18 +16,21 @@ await connectDB()
 app.use(express.json())
 app.use(cors())
 
-const path = require("path");
 
-// Step 1:
-app.use(express.static(path.resolve(__dirname, "./client/build")));
-// Step 2:
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
 
 
 app.use('/api/user',userRouter)
 app.use('/api/image',imageRouter)
+
+
+// Step 1:
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+app.use(express.static(path.resolve(__dirname, "./Client/dist")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./Client/dist", "index.html"));
+});
 
 app.get('/', (req,res) => res.send("API Working"))
 
